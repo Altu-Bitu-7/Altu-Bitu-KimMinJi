cb286a4ed1d066b08bb86055d104d94fa3effaf4
Janice0381 <kmj50911@naver.com>
2024-10-08 15:36:39 +0900

    [DFS_BFS] 10월 8일

---
+++ DFS_BFS/BOJ_2606.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int n, pair_count;
+int graph[100][100] = { 0, };
+bool visited[100] = { false, };
+
+void dfs(int num) {
+    for ( int i = 1; i <= n; i++ ) {
+        if ( graph[num][i] == 1 ) {
+            if ( !visited[i] ) {
+                visited[i] = true;
+                dfs(i);
+            }
+        }
+    }
+}
+
+int main() {
+    int count = 0;
+
+    //입력
+    cin >> n >> pair_count;
+    for ( int i = 0; i < pair_count; i++ ) {
+        int tmp1, tmp2;
+        cin >> tmp1 >> tmp2;
+        graph[tmp1][tmp2] = graph[tmp2][tmp1] = 1;
+    }
+
+    //연산
+    visited[1] = true;
+    dfs(1);
+    for ( int i = 2; i <= n; i++ ) {
+        if ( visited[i] ) {
+            count++;
+        }
+    }
+
+    //출력
+    cout << count << '\n';
+
+    return 0;
+}
